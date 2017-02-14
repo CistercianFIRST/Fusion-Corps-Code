6198c5add52084b80454dfa52e25451ac30b5241
Xavi-Hub <xavihub@gmail.com>
2017-02-14 16:24:10 -0600

    Added rotating speed controllers

--- src/org/usfirst/frc/team6672/robot/Robot.java
+++ src/org/usfirst/frc/team6672/robot/Robot.java
@@ -7,8 +7,6 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.wpilibj.AnalogOutput;
 import edu.wpi.first.wpilibj.Spark;
 
 
@@ -30,12 +28,11 @@ public class Robot extends IterativeRobot {
 
 	/* Speed Control System */
 	double speedLimitMove = 0.6;
-	double speedLimitRotate = -0.6;		// Has to be negative bc the joystick inverts l/r
+	double speedLimitRotate = -0.5;		// Has to be negative bc the joystick inverts l/r
 	
 	/* Sensor Systems */
 	ADXRS450_Gyro spiGyro = new ADXRS450_Gyro();
 	double Kp = 0.03*.35;					//Gyro converter constant (corrected)
-	AnalogOutput mySonar = new AnalogOutput(0);
 	
 	/* Camera Variables */
 	//int IMG_WIDTH = 320;
@@ -45,7 +42,6 @@ public class Robot extends IterativeRobot {
 	
 	/* PWM Stuff */
 	Spark motorLift = new Spark(2);
-	
 	Spark motorGear = new Spark(3);
 		
 	/**
@@ -114,6 +110,7 @@ public class Robot extends IterativeRobot {
 		
 		// Turn 180 command
 		if(stick0.getPOV(0)==180){
+			System.out.println(stick0.getPOV(0));
 			double rotatedHeading = spiGyro.getAngle()+180;
 			while (spiGyro.getAngle()<rotatedHeading){
 				myRobot.drive(0, .5);
@@ -123,9 +120,7 @@ public class Robot extends IterativeRobot {
 				}
 			}
 		}
-		
-		System.out.println((mySonar.getVoltage()*1000)/5120);
-		
+
 		myRobot.arcadeDrive(stick0.getRawAxis(1)*speedLimitMove, stick0.getRawAxis(0)*speedLimitRotate);
 	}
 	
@@ -140,13 +135,13 @@ public class Robot extends IterativeRobot {
 	}	
 	
 	public void cameraInit() {
-        CameraServer.getInstance().startAutomaticCapture().setResolution(1280, 720);
+        CameraServer.getInstance().startAutomaticCapture().setResolution(1024, 576);
         //cam0.setResolution(IMG_WIDTH, IMG_HEIGHT);
 	}
 	
 	public void motorLift() {
 		if(stick0.getRawButton(12)){
-			motorLift.setSpeed(-0.8); // Towards forward
+			motorLift.setSpeed(-1.0); // Towards forward
 		}
 		if(!(stick0.getRawButton(12))){
 			motorLift.setSpeed(0.0);
@@ -155,13 +150,13 @@ public class Robot extends IterativeRobot {
 	
 	public void motorGear() {
 		if(stick0.getRawButton(1)){
-			motorGear.setSpeed(-0.8); // Open gear holder
+			motorGear.setSpeed(-1.0); // Open gear holder
 		}
 		if(stick0.getRawButton(2)){
-			motorGear.setSpeed(0.8); // Close gear holder
+			motorGear.setSpeed(1.0); // Close gear holder
 		}
 		if(stick0.getRawButton(1)==(stick0.getRawButton(2))) {
-			motorGear.setSpeed(0.0); // Towards back
+			motorGear.setSpeed(0.0); // Stop gear holder
 		}
 	}
 	
@@ -177,6 +172,21 @@ public class Robot extends IterativeRobot {
 		}
 		if(stick0.getRawButton(6)){
 			speedLimitMove = 1;	
+		}
+	}
+	
+	public void speedControlRotate() {
+		if(stick0.getRawButton(7)){
+			speedLimitRotate = -0.5;
+		}
+		if(stick0.getRawButton(8)){
+			speedLimitRotate= -0.6;
+		}
+		if(stick0.getRawButton(9)){
+			speedLimitRotate = -0.8;
+		}
+		if(stick0.getRawButton(10)){
+			speedLimitRotate = -1;	
 		}
 	}
 }
