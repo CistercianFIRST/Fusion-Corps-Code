bbd9a2e65c34c41bac88dc12b4d819fbb3ce0f75
Xavi-Hub <xavihub@gmail.com>
2017-02-18 13:12:46 -0600

    Commit right before the practice

--- src/org/usfirst/frc/team6672/robot/Robot.java
+++ src/org/usfirst/frc/team6672/robot/Robot.java
@@ -9,7 +9,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.DigitalInput;
 
 
 /**
@@ -37,7 +36,6 @@ public class Robot extends IterativeRobot {
 	/* Sensor Systems */
 	ADXRS450_Gyro spiGyro = new ADXRS450_Gyro();
 	double Kp = 0.03*.35;				//Gyro converter constant (corrected)
-	DigitalInput motorGearSensor = new DigitalInput(0);
 	
 	/* PWM Stuff */
 	Spark motorLift = new Spark(2);
@@ -154,8 +152,9 @@ public class Robot extends IterativeRobot {
 				}
 			}
 			if(stick0.getRawButton(2)) {
-				//while(timer.get()<1.25){
-				while(motorGearSensor.get()==false){
+				timer.reset();
+				timer.start();
+				while(timer.get()<1.25){
 					motorGear.setSpeed(1.0);
 				}
 			}
